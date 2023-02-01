6bc8dd6312825db151b2cab5a48d40443ce83593
Gabriel Palocz <paloczgabriel@gmail.com>
2023-02-01 17:27:41 -0300

    se agrego el medidor de ventana

--- login1/src/components/authentication/ui-components/card.js
+++ login1/src/components/authentication/ui-components/card.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import useWindowDimensions from '../../../utils/hook/screenSize';
 
 /**
  * This is the card used to wrap the forms content
  */
 
 export const Card = (props) => {
+    const { height } = useWindowDimensions()
 
     return (
-        <div className='d-flex justify-content-center align-items-center p-3' style={{ minWidth: "17rem", minHeight: '100vh', backgroundColor: '#ddedff' }}>
+        <div className='d-flex justify-content-center align-items-center p-3' style={{ minWidth: "17rem", minHeight: `${height}px`, backgroundColor: '#ddedff' }}>
             <div className="card rounded-4 text-center" style={{ width: "24rem", minHeight: '14rem' }}>
                 <div className="card-body" >
                     {props.children}
